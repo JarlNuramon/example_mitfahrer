f77b3f8b0e66515698975a31824f06006bbccfef
VoidfulDawn <saiedsadeghz@gmail.com>
2021-04-21 14:15:12 +0200

    "Go upd!"

--- src/test/java/com/example/mitfahrer/AppTest.java
+++ src/test/java/com/example/mitfahrer/AppTest.java
@@ -1,38 +1,43 @@
 package com.example.mitfahrer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	
+	 
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	    @DisplayName("Play FizzBuzz with number = 1")
+	    @Test
+	    public void testNumber() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		       Assertions.assertTrue(true, "");
+	    }
+
+	    @DisplayName("Play FizzBuzz with number = 3")
+	    @Test
+	    public void testFizz() {
+
+		       Assertions.assertTrue(true, "");
+	    }
+
+	    @DisplayName("Play FizzBuzz with number = 5")
+	    @Test
+	    public void testBuzz() {
+		       Assertions.assertTrue(true, "");
+	    }
+
+	    @DisplayName("Don't Play FizzBuzz with number = 0")
+	    @Test
+	    public void testZero() {
+
+	       Assertions.assertTrue(true, "");
+	    }
+
+	    @AfterEach
+	    public void tearDown() {
+	       
+	    }
 }
